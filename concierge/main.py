c0a49eb3d38a23e18d05e6bd2576ded6a5c5fd2c
Masumi Mashiko <masumi.mashiko@umn.co.jp>
2024-02-07 11:08:45 +0900

    Docker-composeが動くようにする

--- concierge/main.py
+++ concierge/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World concierge !!"}
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8088)
--- handler/main.py
+++ handler/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+import uvicorn
+import schedule
+import time
 
 app = FastAPI()
+interval = 10  # 初期の実行間隔
 
 
 @app.get("/")
@@ -11,3 +15,38 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/service/")
+async def start_request():
+    request_timer()
+    return {"message": "get response from concierge"}
+
+
+def request():
+    global interval
+    if interval < 40:
+        interval += 10  # 実行間隔を10秒増やす
+        schedule.clear()
+        schedule.every(interval).seconds.do(request)
+        print(f"request for concierge : {interval}")
+    else:
+        interval += 5
+        schedule.clear()
+
+
+def request_timer() -> bool:
+    global interval
+    print(f"I'm working start...{interval}")
+
+    schedule.every(interval).seconds.do(request).tag('default')
+    while True:
+        if interval > 40:
+            break
+        schedule.run_pending()
+        time.sleep(1)
+    return True
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8008)
