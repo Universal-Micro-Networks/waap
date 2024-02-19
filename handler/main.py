d9b6ccdc0223d66690c76955f53f405022130ceb
Masumi Mashiko <masumi.mashiko@umn.co.jp>
2024-02-19 21:28:26 +0900

    [handler] Statusで判断してタイマーセットを中止する処理を追加

--- handler/main.py
+++ handler/main.py
@@ -1,10 +1,38 @@
-from fastapi import FastAPI
-import uvicorn
-import schedule
+import os
 import time
 
+import requests
+import schedule
+import uvicorn
+from fastapi import FastAPI
+
 app = FastAPI()
-interval = 10  # 初期の実行間隔
+interval = 1  # 初期の実行間隔
+is_request = False
+request_path = ""
+transaction_id = ""
+
+# 環境変数を取得
+concierge_uri: str = os.getenv("CONCIERGE_URI")
+concierge_check_uri: str = os.getenv("CONCIERGE_CHECK_URI")
+
+
+def set_request_path(path: str):
+    global request_path
+    request_path = path
+
+
+def set_transaction_id(id: str):
+    global transaction_id
+    transaction_id = id
+
+
+def get_request_path() -> str:
+    return request_path
+
+
+def get_transaction_id() -> str:
+    return transaction_id
 
 
 @app.get("/")
@@ -18,13 +46,30 @@ async def say_hello(name: str):
 
 
 @app.get("/service/")
-async def start_request():
-    request_timer()
+async def start_request(path: str):
+    global concierge_uri
+    concierge_uri = str(concierge_uri) + path
+    set_request_path(concierge_uri)
+    request_handler()
     return {"message": "get response from concierge"}
 
 
 def request():
-    global interval
+    global interval, concierge_check_uri, is_request
+    response = requests.get(
+        concierge_check_uri + get_transaction_id(),
+        headers={"Content-Type": "application/json"},
+    )
+    data = response.json()
+    if data["status"] == "FINISHED":
+        print(f"timer finished : {data}")
+        is_request = False
+        interval = 1
+        schedule.clear()
+        return True
+
+    print(f"next timer set : {data}")
+
     if interval < 40:
         interval += 10  # 実行間隔を10秒増やす
         schedule.clear()
@@ -35,13 +80,22 @@ def request():
         schedule.clear()
 
 
-def request_timer() -> bool:
-    global interval
+def request_handler() -> bool:
+    global interval, is_request
     print(f"I'm working start...{interval}")
+    is_request = True
+    response = requests.post(
+        get_request_path(),
+        headers={"Content-Type": "application/json", "x-server-id": "server_id3"},
+        json={"original_path": "admin"},
+    )
+    data = response.json()
+    set_transaction_id(data["transaction_id"])
+    print(data["transaction_id"])
 
-    schedule.every(interval).seconds.do(request).tag('default')
+    schedule.every(interval).seconds.do(request).tag("default")
     while True:
-        if interval > 40:
+        if not is_request:
             break
         schedule.run_pending()
         time.sleep(1)
